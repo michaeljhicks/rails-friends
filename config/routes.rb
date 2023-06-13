f49fddb454e957ff312d770e0d5852c3afd65047
Michael Hicks <hicks.michael04@gmail.com>
2023-06-13 13:20:33 -0600

    Add Bootstrap to application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get 'home/index'
+  root 'home#index'
+  get 'home/about'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
